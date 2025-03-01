d48176556a7a916da0d2c0d4d599f29fe0b1d5d6
leozayr <leonardozayr@gmail.com>
2025-03-01 22:11:35 +0000

    Xadrez Mestre

--- xadrez.c
+++ xadrez.c
@@ -1,61 +1,64 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
-
-int main() 
+void movimentoTorre() 
 {
-    int torre = 0, rainha = 0;
-    //variáveis com os nomes das peças para facilitar o entendimento da movimentação.
+    printf("Torre: Direita\n");
+    printf("Torre: Direita\n");
+    printf("Torre: Direita\n");
+    printf("Torre: Direita\n");
+    printf("Torre: Direita\n");
+}
+ 
+void movimentoRainha() 
+{
+    printf("Rainha: Esquerda\n");
+    printf("Rainha: Esquerda\n");
+    printf("Rainha: Esquerda\n");
+    printf("Rainha: Esquerda\n");
+    printf("Rainha: Esquerda\n");
+    printf("Rainha: Esquerda\n");
+    printf("Rainha: Esquerda\n");
+    printf("Rainha: Esquerda\n");
+}
 
-    while (torre < 5)
+    //Utilização de 'void' para que o código seja mais legivel e simples.
+
+int main() {
+
+    movimentoTorre();
+
+    printf ("\n");
+    printf ("------------------\n");
+    printf ("\n");
+
+    movimentoRainha();
+
+    printf ("------------------\n");
+
+    for (int bispo1 = 0; bispo1 < 5; bispo1++)
     {
-        printf ("Torre: cima.\n");
-        torre++;
-    }
-    //Torre por ser a mais simples foi feita em While.
-
-    printf ("\n");
-    printf ("-----------------------\n");
-    printf ("\n");
-    //criação de espaços entre as movimentações das peças para melhor vizualização do desafio.
-
-    do
-    {
-        printf ("Rainha: esquerda.\n");
-        rainha++;
-    } while (rainha < 8);
-    //rainha por ter o número mais elevado de casas andadas foi feita em Do-While
-    
-    printf ("\n");
-    printf ("-----------------------\n");
-    printf ("\n");
-
-    for (int bispo = 0; bispo < 5; bispo++) 
-    {
-        printf ("Bispo: direita cima.\n");
-    }
-    //Bispo por ser um pouco mais detalhado foi feito em "For".
-
-    printf ("\n");
-    printf ("-----------------------\n");
-    printf ("\n");
-
-    for (int cavalo1; cavalo1 < 1; cavalo1++)
-    {
-        for (int cavalo2; cavalo2 < 2; cavalo2++)
+        for (int bispo2 = 0; bispo2 < 1; bispo2++)
         {
-            printf ("Cavalo: Esquerda\n");
-        }        
-        printf ("Cavalo: Baixo\n");
+            printf ("direita, ");
+        }
+        printf ("cima\n");
     }
-        //Cavalo fazedno movimentação em "L" através do "for" para facilitar a regibilidade do código.
 
-    printf ("\n");
-    printf ("-----------------------\n");
-    printf ("\n");
+    printf ("------------------\n");
 
+    for (int cavalo1 = 0, cavalo2 = 0 ; cavalo1 < 2, cavalo2 < 2; cavalo1++, cavalo2++)
+    {
+        if (cavalo1 == 2) continue;
+    
+            printf ("Cavalo: Cima.\n");
+
+    }
+        printf ("Cavalo: Direita.\n");
+
+        printf ("------------------\n");
 
     return 0;
+    
+    //Código feito através de 'for' para prática pois é o recomendado.
+
 }
